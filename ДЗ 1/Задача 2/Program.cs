eeee5b0b2c25af4c3bfb568e0ba3a70ea5e341c6
Max <mohax28reg.new@gmail.com>
2023-01-29 19:23:16 +0900

    Задание 2 выполнено

---
+++ ДЗ 1/Задача 2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+
+Console.Write("Ввести первое число ");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ввести второе число ");
+int n2 = int.Parse(Console.ReadLine());
+
+int max = n1;
+
+if (max > n2); 
+else max = n2;
+
+Console.Write($"Максимальное число {max}");
