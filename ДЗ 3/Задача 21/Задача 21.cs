2e72b3885c62c7e7724a3536dcc82cbe5335f65b
Max <mohax28reg.new@gmail.com>
2023-02-04 18:41:50 +0900

    задачи 3 семинара выполнены

---
+++ ДЗ 3/Задача 21/Задача 21.cs
@@ -0,0 +1,28 @@
+﻿//Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+
+Console.Write("Введите координату X точки А: ");
+int xa = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату Y точки А: ");
+int ya = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату Z точки А: ");
+int za = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату X точки B: ");
+int xb = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату Y точки B: ");
+int yb = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату Z точки B: ");
+int zb = Convert.ToInt32(Console.ReadLine()!);
+
+double x = Math.Pow((xa - xb), 2);
+double y = Math.Pow((ya - yb), 2);
+double z = Math.Pow((za - zb), 2);
+
+double ab = Math.Sqrt((x + y + z));
+
+Console.WriteLine($"{ab:f2}");
---
+++ ДЗ 3/Задача 23/Задача 23.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+
+int b = 1;
+
+while (b <= a)
+{
+    Console.Write($"{Math.Pow(b, 3)} ");
+    b ++;
+}
