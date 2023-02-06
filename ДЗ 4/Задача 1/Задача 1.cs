501e4d6e2de8dcc78fa0f71b3d33b899d0092f66
Max <mohax28reg.new@gmail.com>
2023-02-06 22:29:26 +0900

    задачи четвёртого семинара выполнены

---
+++ ДЗ 4/Задача 1/Задача 1.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Clear();
+
+Console.Write("Введите число: "); 
+int a = Convert.ToInt32(Console.ReadLine()!);
+
+int sum = 0;
+
+while (a != 0)
+{
+    sum = sum + (a % 10);
+    a = a / 10;
+}
+Console.WriteLine($"Сумма цифр в числе равна {sum}");
---
+++ ДЗ 4/Задача 2/Задача 2.cs
@@ -0,0 +1,27 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+
+int count = 0;
+int prod = 1;
+
+if (n >= 0)
+{
+    while (n > count)
+    {
+        count += 1;
+        prod = prod * count;
+    }
+}
+else
+{
+    while (n < count)
+    {
+        count -= 1;
+        prod = prod * count;
+    }
+}
+
+Console.WriteLine($"{n}! равен {prod}");
