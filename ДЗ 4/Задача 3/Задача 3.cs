6cf0916a19da2e334af72adbc26f1d18a9c360e9
Max <mohax28reg.new@gmail.com>
2023-02-06 22:25:49 +0900

    задачи четвёртого семинара выполнены

---
+++ ДЗ 4/Задача 3/Задача 3.cs
@@ -0,0 +1,34 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+
+Console.Write("Введите число, до которого необходимо проверять условие задача: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+
+int count = 0;
+
+if (a >= 0)
+{
+    while (count <= a)
+    {
+        if (Math.Pow(count, 3) % 2 == 0)
+        {
+            Console.WriteLine($"{count} в кубе равно {Math.Pow(count, 3)}");
+            count++;
+        }
+        else
+            count++;
+    }
+}
+else
+{
+     while (count >= a)
+    {
+        if (Math.Pow(count, 3) % 2 == 0)
+        {
+            Console.WriteLine($"{count} в кубе равно {Math.Pow(count, 3)}");
+            count--;
+        }
+        else
+            count--;
+    }
+} 
